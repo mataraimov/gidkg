1f1b0acf67bbb7e01eb214f2801f60f5427335d5
mataraimov <minuradil@gmail.com>
2023-06-06 20:33:30 +0600

    fix

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import YandexMap from '../modules/components/YMap';
 
 const Test = () => {
-  
   return (
     <>
       page for testing web components
+      <YandexMap />
     </>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
